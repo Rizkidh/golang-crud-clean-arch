a7ab34983a99a71aebbdf13698a6595ce0431ebf
Rizkidh <106050250+Rizkidh@users.noreply.github.com>
2025-04-08 17:06:12 +0700

    Add files via upload

---
+++ golang-crud-clean-arch/config/redis.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+	"golang.org/x/net/context"
+)
+
+func ConnectRedis() *redis.Client {
+	// Gunakan satu variabel ENV REDIS_ADDR
+	addr := os.Getenv("REDIS_ADDR")
+	if addr == "" {
+		addr = "127.0.0.1:6379"
+	}
+
+	password := os.Getenv("REDIS_PASSWORD")
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       0,
+	})
+
+	ctx := context.Background()
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+	}
+
+	fmt.Printf("Connected to Redis at %s!\n", addr)
+	return client
+}
